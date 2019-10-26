8e5edc16b1d11e8775d4b09963b5f7c16cffc71c
Yatharth4599 <37974285+Yatharth4599@users.noreply.github.com>
2019-10-26 10:11:16 +0400

    Third commit

--- src/lottery.js
+++ src/lottery.js
@@ -1,6 +1,6 @@
 import web3 from './web3';
 
-const address = '0x1514D4e8e27b68231eAbA8469778693fbD34bfFd';
+const address = '0x23582004D75cAc7e6D7b3b2FFc04144f682637AE';
 
 const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
